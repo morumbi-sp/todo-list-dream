151a9b3c14d71a2eff04c3c324e5e86c59e81ead
Morumbi <zun0323@gmail.com>
2023-04-22 11:34:35 -0300

    refactoring : delete mainboard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import BackBoard from './components/BackBoard';
 import NavBar from './components/NavBar';
-import MainBoard from './components/MainBoard';
 import Footer from './components/Footer';
+import TodoItem from './components/TodoItem';
 
 export interface ITodo {
   id: string;
@@ -41,7 +41,7 @@ function App() {
         item.id === id
           ? {
               ...item,
-              status: e.target.checked === true ? 'completed' : ' active',
+              status: e.target.checked === true ? 'completed' : 'active',
             }
           : item
       )
@@ -64,12 +64,20 @@ function App() {
         onNavMenu={handleNavMenu}
         navMenu={navMenu}
       />
-      <MainBoard
-        todoList={todoList}
-        onDeleteTodo={handleDeleteTodo}
-        onChangeStatus={handleChangeStatus}
-        navMenu={navMenu}
-      />
+      <div className='min-h-[300px] space-y-4 bg-light-100 px-5 py-5 dark:bg-dark-100'>
+        {todoList
+          .filter((item) =>
+            navMenu === 'all' ? item : item.status === navMenu
+          )
+          .map((item) => (
+            <TodoItem
+              key={item.id}
+              item={item}
+              onDeleteTodo={handleDeleteTodo}
+              onChangeStatus={handleChangeStatus}
+            />
+          ))}
+      </div>
       <Footer onAddTodo={handleAddTodo} />
     </BackBoard>
   );
