0add2afb5e544d55ba789f1f14ef7694037a8fed
Morumbi <zun0323@gmail.com>
2023-04-22 12:16:54 -0300

    css fix

--- src/components/BackBoard.tsx
+++ src/components/BackBoard.tsx
@@ -7,12 +7,8 @@ interface Props {
 
 export default function BackBoard({ children, darkMode }: Props) {
   return (
-    <div
-      className={`${
-        darkMode && 'dark'
-      } mx-auto mt-10  max-w-sm overflow-hidden rounded-2xl `}
-    >
-      {children}
+    <div className={`${darkMode && 'dark'} mx-auto mt-10 max-w-sm  px-4`}>
+      <div className='overflow-hidden rounded-2xl shadow-xl'> {children}</div>
     </div>
   );
 }
